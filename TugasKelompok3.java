6e0c46372a08c1099aa7980f1def2593d11ac01a
Dinda Ragil <dindawardani36@gmail.com>
2022-12-18 21:01:40 +0700

    Initial Commit

---
+++ TugasKelompok3.java
@@ -0,0 +1,12 @@
+//import packages
+import java.util.Scanner;
+
+public class TugasKelompok3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama;
+        System.out.printf("Masukkan nama anda: ");
+        nama = input.nextLine();
+    };
+}
+
